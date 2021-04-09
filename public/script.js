f4a0a94a1a4b75b9f127b419436fde961bbe3f20
Tony Tran <codemonkey713@gmail.com>
2021-04-09 00:42:14 -0500

    added function for checking database framework with event listener for checking online

--- public/script.js
+++ public/script.js
@@ -1,11 +1,29 @@
+let db;
+
 const request = window.indexedDB.open("budget", 1);
 
-request.onsuccess = event => {
-  console.log(request.result);
-};
-
 request.onupgradeneeded = ({ target }) => {
-  const db = target.result;
+  // const db = target.result;
+  db = target.result;
   const objectStore = db.createObjectStore("pending", { autoIncrement: true });
 };
 
+request.onsuccess = e => {
+  console.log(request.result);
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (e) {
+  console.log(`Oops! ${e.target.errorCode}`);
+};
+
+const checkDatabase = () => {
+  console.log('checking on the database');
+
+}
+
+// listen for app to come back online
+window.addEventListener('online', checkDatabase);
