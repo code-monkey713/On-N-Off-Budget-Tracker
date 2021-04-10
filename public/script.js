dae392e3c0383216195a65bf3153a8422c51c6a2
Tony Tran <codemonkey713@gmail.com>
2021-04-10 00:02:57 -0500

    cleaned up repo removed commented codes; remove unnecessary console logs

--- public/script.js
+++ public/script.js
@@ -1,13 +1,9 @@
 let db;
 let budgetVersion;
 
-// const request = window.indexedDB.open("budget", 1);
-// const request = indexedDB.open('budget', budgetVersion || 21);
-const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+const request = indexedDB.open("BudgetDB", budgetVersion || 21);
 
 request.onupgradeneeded = function (e) {
-  console.log('Upgrade needed in IndexDB');
-
   const { oldVersion } = e;
   const newVersion = e.newVersion || db.version;
 
@@ -16,8 +12,7 @@ request.onupgradeneeded = function (e) {
   db = e.target.result;
 
   if (db.objectStoreNames.length === 0) {
-    // db.createObjectStore('pending', { autoIncrement: true });
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
   }
 };
 
@@ -26,63 +21,52 @@ request.onerror = function (e) {
 };
 
 const checkDatabase = () => {
-  console.log('checked on the database');
-  // let transaction = db.transaction(['pending'], 'readwrite');
-  let transaction = db.transaction(['BudgetStore'], 'readwrite');
-  // console.log('this is the transaction: ', transaction);
-
-  // const store = transaction.objectStore('pending');
-  const store = transaction.objectStore('BudgetStore');
-
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
   const getAll = store.getAll();
 
   getAll.onsuccess = function () {
     // If there are items in the store, we need to bulk add them when we are back online
     if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
-        method: 'POST',
+      fetch("/api/transaction/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
         .then((res) => {
           if (res.length !== 0) {
-            // transaction = db.transaction(['pending'], 'readwrite');
-            transaction = db.transaction(['BudgetStore'], 'readwrite');
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+            const currentStore = transaction.objectStore("BudgetStore");
 
-            // const currentStore = transaction.objectStore('pending');
-            const currentStore = transaction.objectStore('BudgetStore');
-
+            // clearing out all the stored items after updating
             currentStore.clear();
-            console.log('Clearing store 🧹');
+            console.log("Clearing store 🧹");
           }
         });
     }
   };
-}
+};
 
 request.onsuccess = (e) => {
   console.log(request.result);
   db = e.target.result;
 
   if (navigator.onLine) {
-    console.log('Backend online! 🗄️');
+    console.log("Backend online! 🗄️");
     checkDatabase();
   }
 };
 
 // function to add to pending item indexedDB
 const saveRecord = (record) => {
-  console.log('Saved record called');
-  // const transaction = db.transaction(['pending'], 'readwrite');
-  const transaction = db.transaction(['BudgetStore'], 'readwrite');
-  // const store = transaction.objectStore('pending');
-  const store = transaction.objectStore('BudgetStore');
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
   store.add(record);
-}
+};
 
 // listen for app to come back online
-window.addEventListener('online', checkDatabase);
+window.addEventListener("online", checkDatabase);
